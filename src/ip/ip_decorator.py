b41cdfbaccd38228fab35fe1198019b3d5809ea5
akkolyth <mx.mazurkiewicz@gmail.com>
2024-08-14 07:35:59 +0000

    Implement IP & UDP

--- src/ip/ip_decorator.py
+++ src/ip/ip_decorator.py
@@ -1,11 +1,129 @@
 """
 transport-layer-protocols
+
+The Internet Protocol is designed for use in interconnected systems of
+packet-switched computer communication networks.
+The internet protocol provides for transmitting blocks of data called
+datagrams from sources to destinations, where sources and destinations
+are hosts identified by fixed length addresses.  The internet protocol
+also provides for fragmentation and reassembly of long datagrams, if
+necessary, for transmission through "small packet" networks.
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |Version|  IHL  |Type of Service|          Total Length         |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |         Identification        |Flags|      Fragment Offset    |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |  Time to Live |    Protocol   |         Header Checksum       |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                       Source Address                          |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                    Destination Address                        |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                    Options                    |    Padding    |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 """
+
+import socket
+import struct
+from enum import Enum, IntEnum
 
 
 class IPDecorator:
-    def __init__(self) -> None:
-        pass
+    class Version(IntEnum):
+        IPV4 = 4
 
-    def decorate(self) -> bytearray:
-        return bytearray()
+    class IHL(IntEnum):
+        DEFAULT = 5
+
+    class Flags(IntEnum):
+        RESERVED = 0
+        DF = 2
+        MF = 1
+
+    class Protocol(IntEnum):
+        ICMP = 1
+        TCP = 6
+        UDP = 17
+
+    class TOS(Enum):
+        DEFAULT = 0
+        LOW_DELAY = 0x10
+        HIGH_THROUGHPUT = 0x08
+        HIGH_RELIABILITY = 0x04
+        MINIMUM_COST = 0x02
+
+    def __init__(
+        self, source_ip: str, dest_ip: str, protocol: Protocol, payload_size: int, ttl: int = 64
+    ) -> None:
+        self.__version = IPDecorator.Version.IPV4
+        self.__ihl = IPDecorator.IHL.DEFAULT
+        self.__tos = IPDecorator.TOS.DEFAULT.value
+        self.__total_length = 20 + payload_size
+        self.__identification = 54321
+        self.__flags = IPDecorator.Flags.DF
+        self.__fragment_offset = 0
+        self.__ttl = ttl
+        self.__protocol = protocol
+        self.__source_ip = source_ip
+        self.__dest_ip = dest_ip
+
+    def decorate(self, payload: bytes) -> bytes:
+        ver_ihl = (self.__version << 4) + self.__ihl
+        flags_frag_offset = (self.__flags << 13) + self.__fragment_offset
+
+        initial_ip_header = self.__build_initial_header(ver_ihl, flags_frag_offset)
+        checksum = self.__calculate_checksum(initial_ip_header)
+        ip_header = self.__build_final_header(ver_ihl, flags_frag_offset, checksum)
+
+        return ip_header + payload
+
+    def __build_initial_header(self, ver_ihl: int, flags_frag_offset: int) -> bytes:
+        ip_header = struct.pack(
+            '!BBHHHBBH4s4s',
+            ver_ihl,
+            self.__tos,
+            self.__total_length,
+            self.__identification,
+            flags_frag_offset,
+            self.__ttl,
+            self.__protocol,
+            b'\x00',
+            socket.inet_aton(self.__source_ip),
+            socket.inet_aton(self.__dest_ip),
+        )
+
+        return ip_header
+
+    def __build_final_header(self, ver_ihl: int, flags_frag_offset: int, checksum: int) -> bytes:
+        ip_header = struct.pack(
+            '!BBHHHBBH4s4s',
+            ver_ihl,
+            self.__tos,
+            self.__total_length,
+            self.__identification,
+            flags_frag_offset,
+            self.__ttl,
+            self.__protocol,
+            checksum,
+            socket.inet_aton(self.__source_ip),
+            socket.inet_aton(self.__dest_ip),
+        )
+
+        return ip_header
+
+    def __calculate_checksum(self, header: bytes) -> int:
+        if len(header) % 2 != 0:
+            header += b'\x00'
+
+        checksum = 0
+        for i in range(0, len(header), 2):
+            word = (header[i] << 8) + header[i + 1]
+            checksum += word
+            checksum = (checksum & 0xFFFF) + (checksum >> 16)
+
+        checksum = ~checksum & 0xFFFF
+
+        return checksum
--- src/udp/udp_decorator.py
+++ src/udp/udp_decorator.py
@@ -1,11 +1,66 @@
 """
 transport-layer-protocols
+
+User Datagram  Protocol  (UDP)  is  defined  to  make  available  a
+datagram   mode  of  packet-switched   computer   communication  in  the
+environment  of  an  interconnected  set  of  computer  networks.
+This protocol  assumes  that the Internet  Protocol  (IP)  is used as the
+underlying protocol. RFC available here: https://datatracker.ietf.org/doc/html/rfc768
+
+ 0      7 8     15 16    23 24    31
++--------+--------+--------+--------+
+|     Source      |   Destination   |
+|      Port       |      Port       |
++--------+--------+--------+--------+
+|                 |                 |
+|     Length      |    Checksum     |
++--------+--------+--------+--------+
+|
+|          data octets ...
++---------------- ...
 """
+
+import struct
 
 
 class UDPDecorator:
-    def __init__(self) -> None:
-        pass
+    _HEADER_SIZE_BYTES = 8
+    _MAX_PAYLOAD_BYTES = 65527
 
-    def decorate(self) -> bytearray:
-        return bytearray()
+    def __init__(self, host_port: int, dest_port: int) -> None:
+        self.__host_port = host_port
+        self.__dest_port = dest_port
+
+    def decorate(self, payload: bytes) -> bytes:
+        if len(payload) > UDPDecorator._MAX_PAYLOAD_BYTES:
+            raise ValueError(
+                f'Payload exceeds max UDP size: {UDPDecorator._MAX_PAYLOAD_BYTES} bytes'
+            )
+
+        packed_ports = self.__pack_port(self.__host_port) + self.__pack_port(self.__dest_port)
+        length = self.__calculate_length(payload)
+        checksum = self.__calculate_checksum(packed_ports + length + b'\x00\x00' + payload)
+        return packed_ports + length + checksum + payload
+
+    def __pack_port(self, port: int) -> bytes:
+        return struct.pack('!H', port)
+
+    def __calculate_length(self, payload: bytes) -> bytes:
+        length = UDPDecorator._HEADER_SIZE_BYTES + len(payload)
+        return struct.pack('!H', length)
+
+    def __calculate_checksum(self, header_and_data: bytes) -> bytes:
+        if len(header_and_data) % 2 != 0:
+            header_and_data += b'\x00'
+
+        checksum = 0
+
+        for i in range(0, len(header_and_data), 2):
+            word = (header_and_data[i] << 8) + header_and_data[i + 1]
+            checksum += word
+            checksum = (checksum & 0xFFFF) + (checksum >> 16)
+
+        checksum = ~checksum & 0xFFFF
+
+        # if checksum results in 0x0000, return 0xFFFF as per UDP standard
+        return struct.pack('!H', checksum if checksum != 0 else 0xFFFF)
